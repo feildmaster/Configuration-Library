5789e16e7869a73c0cff467c685d9d48696dcb35
Travis Ralston <travpc@gmail.com>
2013-01-16 22:39:42 -0700

    Formatting

--- src/main/java/com/feildmaster/lib/configuration/PluginWrapper.java
+++ src/main/java/com/feildmaster/lib/configuration/PluginWrapper.java
@@ -1,24 +1,28 @@
 package com.feildmaster.lib.configuration;
 
 public class PluginWrapper extends org.bukkit.plugin.java.JavaPlugin {
-    private EnhancedConfiguration config;
+	private EnhancedConfiguration config;
 
-    public EnhancedConfiguration getConfig() {
-        if(config == null) {
-            config = new EnhancedConfiguration(this);
-        }
-        return config;
-    }
+	@Override
+	public EnhancedConfiguration getConfig(){
+		if(config == null) {
+			config = new EnhancedConfiguration(this);
+		}
+		return config;
+	}
 
-    public void reloadConfig() {
-        getConfig().load();
-    }
+	@Override
+	public void reloadConfig(){
+		getConfig().load();
+	}
 
-    public void saveConfig() {
-        getConfig().save();
-    }
+	@Override
+	public void saveConfig(){
+		getConfig().save();
+	}
 
-    public void saveDefaultConfig() {
-        getConfig().saveDefaults();
-    }
+	@Override
+	public void saveDefaultConfig(){
+		getConfig().saveDefaults();
+	}
 }
